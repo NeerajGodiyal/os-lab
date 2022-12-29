56166b4430089521b7c8c24824f8074fed71bf32
Neeraj <neerajgodiyal250@gmail.com>
2022-12-29 12:25:19 +0530

    disk

---
+++ fcfs-disk.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    printf("Ayam Dobhal | Section C | University Roll Number 2016685\n");
+    printf("Enter the size of Array of disk: ");
+    int size;
+    int totalmove = 0;
+    int arr[100];
+    scanf("%d", &size);
+    printf("Enter the Track number: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int head;
+    printf("Enter the initial Head: ");
+    scanf("%d", &head);
+    printf("SEEK SEQUENCE IS:\n");
+    for (int i = 0; i < size; i++) {
+        totalmove = totalmove + abs(arr[i] - head);
+        head = arr[i];
+        printf("%d\n", head);
+    }
+    printf("Total Head Movement=%d\n ", totalmove);
+    return 0;
+}
