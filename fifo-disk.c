8fd8c9c3bd6d68caf2b025690cb509d2035443e0
Neeraj <neerajgodiyal250@gmail.com>
2022-12-29 12:26:09 +0530

    disk

---
+++ fifo-disk.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main() {
+    printf("Ayam Dobhal | Section C | University Roll Number 2016685\n");
+    int i, j, n, a[50], frame[10], no, k, avail, count = 0;
+    printf("ENTER THE NUMBER OF PAGES: ");
+    scanf("%d", &n);
+    printf("ENTER THE PAGE NUMBER: ");
+    for (i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+    printf("ENTER THE NUMBER OF FRAMES: ");
+    scanf("%d", &no);
+    for (i = 0; i < no; i++)
+        frame[i] = -1;
+    j = 0;
+    printf("\tref string\t page frames\n");
+    for (i = 1; i <= n; i++) {
+        printf("%d\t\t\t", a[i]);
+        avail = 0;
+        for (k = 0; k < no; k++)
+            if (frame[k] == a[i])
+                avail = 1;
+        if (avail == 0)
+        {
+            frame[j] = a[i];
+            j = (j + 1) % no;
+            count++;
+            for (k = 0; k < no; k++)
+                printf("%d\t", frame[k]);
+        }
+        printf("\n");
+    }
+    printf("Page Fault is %d\n", count);
+    return 0;
+}
